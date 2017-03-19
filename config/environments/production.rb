948975273d0f25b299a7d0a61597d993a1dd57d7
jboltz88 <boltz.justin@gmail.com>
2017-03-18 19:32:57 -0500

    Change smtp from gmail to sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,12 +91,12 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: :plain,
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    address:                'smtp.sendgrid.net',
+    port:                   '2525',
+    authentication:         :plain,
+    user_name:              ENV['SENDGRID_USERNAME'],
+    password:               ENV['SENDGRID_PASSWORD'],
+    domain:                 'heroku.com',
+    enable_starttls_auto:   true
   }
 end
