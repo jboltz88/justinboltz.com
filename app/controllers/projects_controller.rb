a2ba7c0e11ceb7e5324996c0d2c39980f5901c5e
jboltz88 <boltz.justin@gmail.com>
2017-03-25 21:29:22 -0500

    Complete skeleton for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,9 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
   end
 
   def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def destroy
+    @project = Project.find(params[:id])
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
   validates :title, length: { minimum: 2}, presence: true
+  validates :url, length: { minimum: 2}, presence: true
+  validates :description, length: { minimum: 4}, presence: true
   validates :body, length: { minimum: 20 }, presence: true
 end
