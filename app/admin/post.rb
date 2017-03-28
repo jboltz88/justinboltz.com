1024a50e853f41993e3e50458b7970d9ffbf0c8c
jboltz88 <boltz.justin@gmail.com>
2017-03-28 17:47:10 -0500

    Add images to posts

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -2,14 +2,16 @@ ActiveAdmin.register Post do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :title, :body, :date
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-  actions :all
+  permit_params :title, :body, :date, :image, :image_file_name, :image_content_type, :image_file_size, :image_updated_at
+
+
+  form do |f|
+    f.inputs "Project Details" do
+      f.input :title
+      f.input :body
+      f.input :date
+      f.input :image, :required => false, :as => :file
+    end
+    f.actions
+  end
 end
