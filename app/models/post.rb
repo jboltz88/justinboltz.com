779663424c58e00009439847718cb60178288344
jboltz88 <boltz.justin@gmail.com>
2017-03-30 17:42:17 -0500

    Complete working About Me view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   # has_many :comments
-  has_attached_file :image, styles: { large: "800x800>", thumb: "300x300>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { large: "600x600>", thumb: "300x300>" }, default_url: "/images/:style/missing.png"
   do_not_validate_attachment_file_type :image
-  
+
   default_scope { order('date DESC') }
 
   validates :title, length: { minimum: 2}, presence: true
