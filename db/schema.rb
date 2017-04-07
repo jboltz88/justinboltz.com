bad0855d37a4b73138288e8eb47ab63656902d47
jboltz88 <boltz.justin@gmail.com>
2017-04-06 19:00:25 -0500

    Format changes to index views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328212244) do
+ActiveRecord::Schema.define(version: 20170406182007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20170328212244) do
     t.string   "screenshot_content_type"
     t.integer  "screenshot_file_size"
     t.datetime "screenshot_updated_at"
+    t.string   "github_url"
   end
 
 end
